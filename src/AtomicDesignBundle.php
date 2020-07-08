25e32614b4c329cd0080982b5d78f416a089ca58
Quentin Machard <quentin.machard@gmail.com>
2020-07-08 21:20:42 +0200

    First Commit

---
+++ src/AtomicDesignBundle.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace QuentinMachard\Bundle\AtomicDesignBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class AtomicDesignBundle extends Bundle
+{
+
+}
