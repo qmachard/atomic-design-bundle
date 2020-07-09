2a845d9bc83d4c57396afab99c3bb8d7d452125b
Quentin Machard <quentin.machard@gmail.com>
2020-07-10 00:09:00 +0200

    :white_check_mark: Adding full Card Example

--- tests/app/tests/AtomicDesign/Components/Molecules/CardComponent.php
+++ tests/app/tests/AtomicDesign/Components/Molecules/CardComponent.php
@@ -16,21 +16,32 @@ class CardComponent extends Component
     public function getStories(): array
     {
         return [
-            'Primary' => 'storyPrimary',
+            'Default' => 'default',
+            '+ image' => 'withImage',
         ];
     }
 
-    public function getDefaultStory(): string
+    public function default(): string
     {
-        return 'storyPrimary';
+        return $this->render('@components/molecules/card/card.html.twig', [
+            'props' => [
+                'title' => 'Card title',
+                'text' => 'Some quick example text to build on the card title and make up the bulk of the card\'s content.',
+            ]
+        ]);
     }
 
-    public function storyPrimary(): string
+    public function withImage(): string
     {
-        return $this->render('@components/atoms/button/button.html.twig', [
+        return $this->render('@components/molecules/card/card.html.twig', [
             'props' => [
-                'label' => 'Primary',
-            ],
+                'image' => [
+                    'src' => 'http://placekitten.com/300/200',
+                    'alt' => 'A cat.',
+                ],
+                'title' => 'Card title',
+                'text' => 'Some quick example text to build on the card title and make up the bulk of the card\'s content.',
+            ]
         ]);
     }
 }
