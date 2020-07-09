0154c7250ae86da262b4e10e4d02aec44c21ea6a
Quentin Machard <quentin.machard@gmail.com>
2020-07-09 22:20:49 +0200

    :white_check_mark: Adding Webpack Encore and create first component button

--- tests/app/config/bundles.php
+++ tests/app/config/bundles.php
@@ -15,4 +15,5 @@ return [
     QuentinMachard\Bundle\AtomicDesignBundle\AtomicDesignBundle::class => ['dev' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     Knp\Bundle\MenuBundle\KnpMenuBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
