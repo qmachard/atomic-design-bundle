590bf71da0af880941c6b6fe322cd84cc55dd606
Quentin Machard <quentin.machard@gmail.com>
2021-09-14 10:31:03 +0200

    :white_check_mark: Adding symfony versions check

---
+++ tests/5.3/config/bundles.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    QuentinMachard\Bundle\AtomicDesignBundle\AtomicDesignBundle::class => ['dev' => true, 'test' => true],
+];
